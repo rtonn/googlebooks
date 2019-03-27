b1deafeaa05c99e22e2503ff19da739939b003af
rtonn <rtonn09umn@gmail.com>
2019-03-27 18:23:20 -0500

    Updates

---
+++ precache-manifest.8405aa99108f50fa9b0891abcf023cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/googlebooks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97c07a0dbdb079a0dd9d",
+    "url": "/googlebooks/static/js/runtime~main.fb3a3354.js"
+  },
+  {
+    "revision": "5aa4786306a9117eeffa",
+    "url": "/googlebooks/static/js/main.d2ef4cbd.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/googlebooks/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5aa4786306a9117eeffa",
+    "url": "/googlebooks/static/css/main.f5129e78.chunk.css"
+  },
+  {
+    "revision": "db777b78b3843378e8335a1ffe8618aa",
+    "url": "/googlebooks/index.html"
+  }
+];
