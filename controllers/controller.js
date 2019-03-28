f2d339670fd7d9ddbe43e0829abeab20661e4e22
rtonn <rtonn09umn@gmail.com>
2019-03-28 18:00:28 -0500

    updates

---
+++ controllers/controller.js
@@ -0,0 +1,36 @@
+const db = require("../models"); 
+
+module.exports = {
+    
+    findAll: (req, res) => {
+        db.Book.find(req.query)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findById: (req, res) => {
+        db.Book.findById(req.params.id)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+
+    create: (req, res) => {
+        db.Book.create(req.body)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+
+    update: (req, res) => {
+        db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    },
+
+    remove: (req, res) => {
+        db.Book.findById(req.params.id)
+            .then(dbBook => dbBook.remove())
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+    }
+    
+};
